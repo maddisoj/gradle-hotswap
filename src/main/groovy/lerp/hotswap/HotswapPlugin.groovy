3389e0be39d002bdb65c374de690abc12aec50b7
James Maddison <james@rarge.co.uk>
2014-09-13 17:14:18 +0100

    Restructered and spawn task

--- src/main/groovy/lerp/hotswap/HotswapPlugin.groovy
+++ src/main/groovy/lerp/hotswap/HotswapPlugin.groovy
@@ -1,25 +1,26 @@
 package lerp.hotswap;
 
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
-import org.gradle.api.file.FileCollection;
-import org.gradle.api.plugins.JavaPlugin;
-import org.gradle.api.plugins.JavaPluginConvention;
-import org.gradle.api.tasks.SourceSet;
-import org.gradle.api.tasks.StopExecutionException;
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.file.FileCollection
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.JavaPluginConvention
+import org.gradle.api.tasks.SourceSet
+import org.gradle.api.tasks.StopExecutionException
+
+import lerp.hotswap.tasks.Spawn
+import lerp.hotswap.extensions.HotswapExtension
 
 class HotswapPlugin implements Plugin<Project> {
-    private Project project
-    private HotswapExtension extension
+    static final String HOTSWAP_SPAWN_TASK_NAME = "JavaSpawn"
 
+    @Override
     void apply(Project project) {
         if(!project.plugins.hasPlugin(JavaPlugin)) {
-            throw new StopExecutionException("Project must apply java plugin");
+            throw new StopExecutionException("Project must apply java plugin")
         }
 
-        this.project   = project
-        this.extension = project.extensions.create("hotswap", HotswapExtension)
-
-        project.tasks.create("JavaSpawn", JavaSpawn)
+        project.extensions.create("hotswap", HotswapExtension, project)
+        project.tasks.create(HOTSWAP_SPAWN_TASK_NAME, Spawn)
     }
 }
